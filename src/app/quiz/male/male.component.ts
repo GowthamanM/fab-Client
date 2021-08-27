36dfe0d5bfb6100cbacfad49c10e2d697c258eeb
Gowthaman T <happygowthaman@gmail.com>
2021-08-28 01:00:57 +0530

    page by page question functionality added

--- src/app/quiz/male/male.component.ts
+++ src/app/quiz/male/male.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-male',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaleComponent implements OnInit {
 
-  constructor() { }
+  id: number = 1;
+  backBtnActive: boolean = false;
+  nextBtnActive: boolean = true;
+  count: number = 7;
+  arrIndex: number = 0;
+
+  question = ['Question 1','Question 2', 'Question 3', 'Question 4', 'Question 5', 'Question 6', 'Question 7'];
+
+  constructor(private activeRoute: ActivatedRoute, private route: Router) {
+  }
 
   ngOnInit(): void {
+    this.activeRoute.params.subscribe(routeParams => {
+      this.id = +routeParams.id;
+      this.checkRouteChanges();
+    });
+  }
+
+  checkRouteChanges() {
+    this.arrIndex = this.id - 1;
+    
+    if(this.id > 1) {
+      this.backBtnActive = true;
+    }
+    else {
+      this.backBtnActive = false;
+    }
+
+    if(this.id == this.count) {
+      this.nextBtnActive = false;
+    }
+    else if(this.id > this.count) {
+      this.route.navigate(['page-not-found']);
+    }
+    else {
+      this.nextBtnActive = true;
+    }
+  }
+
+  goBack() {
+    let currentRoute = this.id;
+    let prevRoute = currentRoute - 1;
+    this.route.navigate(['quiz/male/' + prevRoute]);
+  }
+
+  goNext() {
+    let currentRoute = this.id;
+    let nextRoute = currentRoute + 1;
+    this.route.navigate(['quiz/male/' + nextRoute]);
   }
 
 }
