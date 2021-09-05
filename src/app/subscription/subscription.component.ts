7f4048723eb9db07ca72132fdf636f4cfe6286af
GowthamanM <gowthamancode@gmail.com>
2021-09-05 06:20:46 +0530

    Subscription Added

--- src/app/subscription/subscription.component.ts
+++ src/app/subscription/subscription.component.ts
@@ -42,7 +42,7 @@ premiumPlanData :any = {
 }
 
 options = { 
-  key: 'rzp_test_COSZxgtxNusnuj', 
+  key: 'rzp_live_S9n4NwIEsUD5Pv', 
   order_id: '', 
   name: 'Fabrae', 
   description: 'Monthly Test Plan', 
