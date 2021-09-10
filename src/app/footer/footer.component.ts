03f17932d84b600f548f1ab75e60483837a69d35
Gowthaman T <happygowthaman@gmail.com>
2021-09-10 15:13:26 +0530

    whatsapp feature completed

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  presentYear: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.presentYear = new Date().getFullYear();
+    console.log(this.presentYear);
+  }
+
+  whatsapp() {
+    var media = window.matchMedia("(max-width: 576px)");
+
+    // If media query matches
+    if (media.matches) {
+      var url = 'https://wa.me/+919566615244/?text=';
+    } else {
+      var url = 'https://web.whatsapp.com/send?phone=+919566615244&text=';
+    }
+
+    //define the message text
+    var text = 'Hey Specialist!, I need your help \n';
+
+    //encode the text
+    var encodedText = encodeURIComponent(text);
+
+    console.log(url + encodedText);
+
+    var finalUrl = url + encodedText;
+
+    window.open(finalUrl,'_blank');
   }
 
 }
