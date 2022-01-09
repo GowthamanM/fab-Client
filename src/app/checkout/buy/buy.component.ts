92875b6d7f15ffbb716c07bf4bc355a3cf007a66
Gowthaman T <happygowthaman@gmail.com>
2022-01-10 02:49:08 +0530

    feat(buy): buy page ui completed - cart ui refactored small change

--- src/app/checkout/buy/buy.component.ts
+++ src/app/checkout/buy/buy.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-buy',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buy.component.scss']
 })
 export class BuyComponent implements OnInit {
+  data:any = [];
+  subTotal:any = 0;
+  total = 0;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  public prdQuantity = 1;
+
+  constructor(
+    private userService:UserService,
+    private fb: FormBuilder,
+  ) {
   }
 
+  ngOnInit(): void {
+      this.userService.getUserData().subscribe(data=>{
+        if(data.User.hasOwnProperty("relatedTo")){
+          this.data = data.User.relatedTo.cart;
+          for(let i=0;i<this.data.length;i++){
+            console.log(this.data[i]);
+
+            this.subTotal += parseInt(this.data[i].price);
+          }
+          this.total= this.subTotal+50;
+
+        }
+      })
+  }
+
+  removeProduct(product: any) {
+
+  }
+
+  deliveryForm = this.fb.group({
+    inputName: [''],
+    inputEmail: [''],
+    inputAddress: [''],
+    inputCity: [''],
+    inputState: [''],
+    inputZip: [''],
+    inputPhone: [''],
+    inputCountry: [''],
+  });
+
+  showData() {
+    console.log(this.deliveryForm);
+  }
+
+  paynow() {
+    
+  }
 }
